2265a53038694baba8c6e6d7f1ab859117525b69
weroz07 <daniilparakhin2007@gmail.com>
2025-12-14 18:39:11 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include<locale.h>
+
+int year;
+
+main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("Введите год");
+	scanf("%d", &year);
+
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+
+		printf("Год %d высокосный", year);
+
+	}
+	else {
+
+		printf("Год %d не высокосный", year);
+
+	}
+
+	return 0;
+
+}
