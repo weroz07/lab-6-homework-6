0d706691f1690ebabceaf6b56db6eb44e88d9d9f
weroz07 <daniilparakhin2007@gmail.com>
2025-12-15 05:02:25 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,26 +1,25 @@
-
-
 //6 2
 #define _CRT_SECURE_NO_DEPRECATE
-#include<stdio.h>
-#include<locale.h>
-#include<math.h>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define p 2.6
 
-
-main()
+double x;
+float y, a, b;
+int a1, b1, c;
+void main()
 {
-	double x, F1, F2;
-
-	setlocale(LC_ALL, "RUS");
-
-	puts("Ââåäèòå ÷èñëî x");
-	scanf("%lf", &x);
-	F1 = -1 * pow(x, 2) + x - 9;
-	F2 = 1 / (pow(x, 4) - 6);
-	printf("%.2lf", x >= 8 ? F1 : F2);
-
-
-	return 0;
-
-
+    setlocale(LC_ALL, "RUS");
+    puts("Введите параметр X:");
+    scanf("%e", &x);
+    a = powf(p, 3) + pow(x, 3);
+    b = expf(powf((p + x), 0.5));
+    y = powf(b, 3) / powf(a, 2);
+    printf("Результат: %.2f\n", y);
+    a1 = (int)a;
+    b1 = (int)b;
+    c = (int)y;
+    printf("a = %d, b = %d, c = %d\n", a1, b1, c);
+    
 }
