a8efb2d794de53a9bc007d8b96143e14764b28af
weroz07 <daniilparakhin2007@gmail.com>
2025-12-15 05:03:56 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,26 +1,26 @@
+//6 1
 #define _CRT_SECURE_NO_DEPRECATE
 #include<stdio.h>
 #include<locale.h>
 
 int year;
 
-main()
+int main()
 {
 	setlocale(LC_ALL, "RUS");
-	puts("Введите год");
-	scanf("%d", &year);
+	puts("Р’РІРµРґРёС‚Рµ РіРѕРґ");
+	scanf("%d", &year );
 
 	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+		
+		printf("Р“РѕРґ %d РІС‹СЃРѕРєРѕСЃРЅС‹Р№", year);
 
-		printf("Год %d высокосный", year);
+	} else {
 
-	}
-	else {
-
-		printf("Год %d не высокосный", year);
-
+		printf("Р“РѕРґ %d РЅРµ РІС‹СЃРѕРєРѕСЃРЅС‹Р№", year);
+	
 	}
 
 	return 0;
-
+	
 }
